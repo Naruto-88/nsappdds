46d04443435f0b18d5bf5a7bcbc5d61b39c85dc0
Naruto-88 <hiimnaru7o@gmail.com>
2026-08-25 10:28:31 +0530

    Fix root app.js clean startup

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const path = require('path');
-const fs = require('fs');
-
-// Try loading .env from multiple locations
-const envPaths = [
-  path.join(__dirname, '.env'),
-  path.join(__dirname, 'server', '.env')
-];
-
-for (const p of envPaths) {
-  if (fs.existsSync(p)) {
-    require('dotenv').config({ path: p });
-  }
-}
-
 require('./server/dist/main.js');
